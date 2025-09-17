4bce949a0cf5b128671862dd80011568588d28b9
bm3575134-tech <bm3575134@gmail.com>
2025-09-18 02:12:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,9 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/campus-craft-co/',   // ⚠ यहाँ अपने repo का नाम डालना है
+})
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
